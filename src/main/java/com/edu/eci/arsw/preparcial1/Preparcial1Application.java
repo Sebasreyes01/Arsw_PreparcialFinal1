967d3b5290ac9fac5107a2fea485451db649838d
Sebastián Reyes <sebastianreyes01@hotmail.com>
2018-11-29 16:40:04 -0500

    First commit

---
+++ src/main/java/com/edu/eci/arsw/preparcial1/Preparcial1Application.java
@@ -0,0 +1,12 @@
+package com.edu.eci.arsw.preparcial1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Preparcial1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Preparcial1Application.class, args);
+	}
+}
